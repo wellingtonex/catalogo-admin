51c540ff7e8cffcc6f158b4873b57a4f0ad9d719
Wellington Siqueira <wellingtonex@gmail.com>
2024-09-03 23:48:17 -0300

    adicionando spring ao infrastructure

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,10 +1,19 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
-import com.fullcycle.admin.catalogo.application.UseCase;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-//        System.out.println(new UseCase().execute());
+        System.out.println("Hello World");
+        SpringApplication.run(WebServerConfig.class);
     }
 }
